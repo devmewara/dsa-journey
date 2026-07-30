eb247e3d9d5bff6579a8a35254ed824429f79e3c
devmewara <devmewara9705@gmail.com>
2026-07-30 17:31:08 +0530

    Creating and traversing a binary tree

--- binary-tree/00-binary-tree-implementation.cpp
+++ binary-tree/00-binary-tree-implementation.cpp
@@ -31,6 +31,34 @@ Node* buildTree(Node* root){
     return root;
 }
 
+// Build binary tree from Level Order
+void buildFromLevelOrder(Node* &root){
+    queue<Node*> q;
+    cout<<"Enter data for root: ";
+    int data;
+    cin>>data;
+    root=new Node(data);
+    q.push(root);
+    while (!q.empty()){
+        Node* temp=q.front();
+        q.pop();
+        cout<<"Enter left node for "<<temp->data<<endl;
+        int leftData;
+        cin>>leftData;
+        if (leftData!=-1){
+            temp->left=new Node(leftData);
+            q.push(temp->left);
+        }
+        cout<<"Enter right node for "<<temp->data<<endl;
+        int rightData;
+        cin>>rightData;
+        if (rightData!=-1){
+            temp->right=new Node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
 // Level order traversal
 void levelOrderTraversal(Node* root){
     queue<Node*> q;
@@ -57,11 +85,63 @@ void levelOrderTraversal(Node* root){
     }
 }
 
+// Pre-Order traversal
+void preOrderTraversal(Node* root){
+    if (root==NULL){
+        return ;
+    }
+    cout<<root->data<<" ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+
+// In-Order Traversal
+void inOrderTraversal(Node* root){
+    if (root==NULL){
+        return ;
+    }
+    inOrderTraversal(root->left);
+    cout<<root->data<<" ";
+    inOrderTraversal(root->right);
+}
+
+// Post-Order Traversal
+void postOrderTraversal(Node* root){
+    if (root==NULL){
+        return ;
+    }
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout<<root->data<<" ";
+}
+
+
 int main(){
-    Node* root=NULL;
+    Node* root1=NULL;
     // Creating tree
-    root=buildTree(root);
-    // Level order
-    levelOrderTraversal(root);
+    root1=buildTree(root1);
+    // Printing
+    cout<<endl<<"Level order traversal: "<<endl;
+    levelOrderTraversal(root1);
+    cout<<endl<<"Pre-Order traversal: "<<endl;
+    preOrderTraversal(root1);
+    cout<<endl<<"In-Order traversal: "<<endl;
+    inOrderTraversal(root1);
+    cout<<endl<<"Post-Order traversal: "<<endl;
+    postOrderTraversal(root1);
+
+    // Creating another tree
+    Node* root2=NULL;
+    buildFromLevelOrder(root2);
+    // Printing
+    cout<<endl<<"Level order traversal: "<<endl;
+    levelOrderTraversal(root2);
+    cout<<endl<<"Pre-Order traversal: "<<endl;
+    preOrderTraversal(root2);
+    cout<<endl<<"In-Order traversal: "<<endl;
+    inOrderTraversal(root2);
+    cout<<endl<<"Post-Order traversal: "<<endl;
+    postOrderTraversal(root2);
+
     return 0;
 }
