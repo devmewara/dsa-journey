7d24e8f2c38d61f00f6efd970b614378dbc7ff1d
devmewara <devmewara9705@gmail.com>
2026-08-01 14:53:42 +0530

    Diagonal traversal of tree

---
+++ binary-tree/14-diagonal-tree-traversal.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+
+class Solution {
+  public:
+    vector<int> diagonal(Node *root) {
+        vector<int> ans;
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty()){
+            Node* temp=q.front();
+            q.pop();
+            while (temp!=NULL){
+                ans.push_back(temp->data);
+                if (temp->left){
+                    q.push(temp->left);
+                }
+                temp=temp->right;
+            }
+        }
+        return ans;
+    }
+};
+
+
+// Driver code
+
+void buildFromLevelOrder(Node* &root){
+    queue<Node*> q;
+    cout<<"Enter data for root: ";
+    int data;
+    cin>>data;
+    root=new Node(data);
+    q.push(root);
+    while (!q.empty()){
+        Node* temp=q.front();
+        q.pop();
+        cout<<"Enter left node for "<<temp->data<<endl;
+        int leftData;
+        cin>>leftData;
+        if (leftData!=-1){
+            temp->left=new Node(leftData);
+            q.push(temp->left);
+        }
+        cout<<"Enter right node for "<<temp->data<<endl;
+        int rightData;
+        cin>>rightData;
+        if (rightData!=-1){
+            temp->right=new Node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
+
+int main(){
+    Solution s;
+    Node* root=NULL;
+    buildFromLevelOrder(root);
+    vector<int> ans=s.diagonal(root);
+    for (int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
